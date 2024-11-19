28ecbc393e50bd38ab69b3043274cdf624a71ddb
Prajjwal Singh <prajjwals203@gmail.com>
2024-11-19 16:57:55 +0530

    Initial commit for Portfolio website

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './section/Navbar'
+
+const App = () => {
+  return (
+    <main className="max-w-7xl mx-auto">
+
+      <Navbar />
+      {/* <h1 className="text-2xl text-white underline">hello Three.js</h1> */}
+
+    </main>
+
+
+  )
+}
+
+export default App
