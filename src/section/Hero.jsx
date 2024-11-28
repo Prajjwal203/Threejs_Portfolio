7259237ba8b5a3f6d2d987d4906478edf971e9b0
Prajjwal Singh <prajjwals203@gmail.com>
2024-11-28 16:08:23 +0530

    Hero Section complete fn

--- src/section/Hero.jsx
+++ src/section/Hero.jsx
@@ -7,6 +7,12 @@ import CanvasLoader from '../components/CanvasLoader';
 import { calculateSizes } from '../constants';
 // import { useControls } from 'leva';
 import { useMediaQuery } from 'react-responsive';
+import Target from '../components/Target';
+import ReactLogo from '../components/ReactLogo';
+import Cube from '../components/Cube';
+import Rings from '../components/Rings';
+import HeroCamera from '../components/HeroCamera';
+import Button from '../components/Button';
 
 const Hero = () => {
   // const x = useControls('HackerRoom', {
@@ -81,22 +87,34 @@ const Hero = () => {
           <Suspense fallback={<CanvasLoader />}>
 
             <PerspectiveCamera makeDefault position={[0, 0, 20]} />
-
+            <HeroCamera>
             <HackerRoom 
               // position={[2, -10.5, 2]} 
               // scale={isMobile ? 0.08 : 0.1} 
               position={sizes.deskPosition}
               rotation={[0, -Math.PI, 0]} 
               scale={sizes.deskScale}
-
               />
+              </HeroCamera>
+
+              <group>
+                <Target position={sizes.targetPosition} />
+                <ReactLogo position={sizes.reactLogoPosition} />
+                <Cube position={sizes.cubePosition} />
+                <Rings position={sizes.ringPosition} />
+              </group>
 
               <ambientLight intensity={1} />
               <directionalLight position={[10, 10, 10]} intensity={0.5} />
 
           </Suspense>
         </Canvas>
+      </div>
 
+      <div className='absolute bottom-7 left-0 right-0 w-full z-10 c-space'>
+        <a href='#contact' className='w-fit'>
+          <Button name="Let's work together" isBeam containerClass='sm:w-fit w-full sm:min-w-96' />
+        </a>
       </div>
 
     </section>
